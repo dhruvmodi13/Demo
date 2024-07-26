1c0d4b214a04912671b027b53a103a1a25dbb0f6
dhruvmodi13 <58764154+dhruvmodi13@users.noreply.github.com>
2024-07-26 10:41:57 -0700

    Create CosmosDB.IPUpdate.ps1

---
+++ Scripts/CosmosDB.IPUpdate.ps1
@@ -0,0 +1,54 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string] $ResourceGroupName,
+
+    [Parameter(Mandatory = $true)]
+    [string] $AccountName,
+
+    [Parameter(Mandatory = $true)]
+    [string[]] $NewIpRanges
+)
+
+# Get the Cosmos DB account object
+$cosmosObj = Get-AzCosmosDBAccount -Name $AccountName -ResourceGroupName $ResourceGroupName
+
+# Check if IpRules is null or empty and initialize an empty array if true
+if ($null -eq $cosmosObj.IpRules -or $cosmosObj.IpRules.Count -eq 0) {
+    $currentIpRangesArray = @()
+    Write-Output "No existing IP rules found. Initializing with an empty array."
+} else {
+    # Retrieve the current IP rules and convert to an array of IP strings
+    $currentIpRangesArray = $cosmosObj.IpRules | Select-Object -ExpandProperty IPAddressOrRangeProperty
+}
+
+# Initialize a list to store all updated IP ranges
+$updatedIpRangesArray = $currentIpRangesArray.Clone()
+$changesMade = $false  # Flag to track if new IPs are added
+
+# Loop through each new IP range and add it to the list if it is not already included
+foreach ($newIpRange in $NewIpRanges) {
+    if (-not $updatedIpRangesArray.Contains($newIpRange)) {
+        $updatedIpRangesArray += $newIpRange
+        Write-Output "Added IP range $newIpRange to update list"
+        $changesMade = $true  # Set flag to true indicating a change was made
+    } else {
+        Write-Output "IP range $newIpRange already exists in Cosmos DB account $AccountName"
+    }
+}
+
+# Check if changes were made and update the Cosmos DB account
+if ($changesMade) {
+    # Update the Cosmos DB account with the new IP rules
+    $cosmosObj = Get-AzCosmosDBAccount -Name $AccountName -ResourceGroupName $ResourceGroupName
+    if ($cosmosObj.ProvisioningState -ne "Updating")
+    {
+        Update-AzCosmosDBAccount -Name $AccountName -ResourceGroupName $ResourceGroupName -IpRule $updatedIpRangesArray
+        Write-Output "Updated IP ranges for Cosmos DB account $AccountName"
+    }
+    else 
+    {
+        Write-Output "There is already an operation in progress which requires exclusive lock on this Cosmos: $AccountName . Please retry the operation after sometime"
+    }
+} else {
+    Write-Output "No new IP ranges added; no update necessary."
+}
