e93af2de881eaacb7f696586bcfba14d0d9cac77
dhruvmodi13 <58764154+dhruvmodi13@users.noreply.github.com>
2024-10-17 11:08:28 -0700

    Update and rename FrontEnd.Automate.ps1 to FrontEnd.Automate1.ps1

--- Scripts/FrontEnd.Automate1.ps1
+++ Scripts/FrontEnd.Automate1.ps1
@@ -13,7 +13,7 @@ Param(
 $indexDocument = "somehtml.html"
 
 Write-Output "Enabling Static website for $storageAccount_name"
-$context = (Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccount_name).Context
+$context = New-AzStorageContext -StorageAccountName -Name $storageAccount_name -UseConnectedAccount
 Enable-AzStorageStaticWebsite -Context $context -IndexDocument $indexDocument
 Write-Output "Enabled Static website for $storageAccount_name"
 
